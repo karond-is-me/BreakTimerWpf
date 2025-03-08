9e9147b6996313dfb45a5cb75587d6156d2f96b7
karond <dingyaohui.g@outlook.com>
2025-03-09 00:01:18 +0800

    防止重复启动

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,25 @@ namespace BreakTimer
     /// </summary>
     public partial class App : Application
     {
+        private Mutex mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            base.OnStartup(e);
+
+            mutex = new Mutex(true, "breaktimerwpf", out bool createdNew);
+            // 如果Mutex不是新创建的，表示已有实例正在运行
+            if (!createdNew)
+            {
+                MessageBox.Show("该应用程序已经在运行。");
+                Application.Current.Shutdown(); // 关闭新的实例
+                return;
+            }
+            else
+            {
+                // 如果是新创建的Mutex，添加一个处理程序来在应用程序退出时释放Mutex
+                Application.Current.Exit += (s, ee) => mutex.Dispose();
+            }
+        }
     }
 }
